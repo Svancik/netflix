18bec8e45ea8a01acb944bbf25aa5e0b29d4e905
Svancik <thesvancik@gmail.com>
2022-06-05 13:06:20 +0200

    UI - navbar finished

--- Tutorial.jsx
+++ Tutorial.jsx
@@ -2,7 +2,8 @@
 0) Inicializace nového REACT v18 projektu:
 Kód níže nainstaluje nejaktuálnější verzi reactu:
     npx create-react-app "my-netflix"
-
+    
+-----------------------------------------------------------------------------------------------------------------------------------
 A1) Instalace a používání SASS - náhrada stylizace za CSS
 a) npm add sass
 b) Vytvoříme nový soubor app.scss (SASS soubor).
@@ -13,7 +14,49 @@ c) Nadefinujeme si hlavní barvy v "app.scss" jako proměnné - k těmto uložen
         }/*
 d) K této proměnné "main-color" budeme přistupovat uvnitř "home.scss" souboru pro "Home.jsx"
 
+-----------------------------------------------------------------------------------------------------------------------------------
 B1) Instalace a použití Material-UI ikon (od v18)
 a) npm install @mui/material @emotion/react @emotion/styled --legacy-peer-deps
 b) npm install @mui/icons-material --legacy-peer-deps
+c) Napíšeme vždy název ikony do TAGU a automaticky se nám naimportuje soubor.
 
+
+-----------------------------------------------------------------------------------------------------------------------------------
+C1) DROPDOWN Navbar menu
+"PŘIŠPENDLENÍ DIVU"
+a) Vytvoříme si Navbar ve kterém budou položky seznamu uvnitř <div className="options"/>  */
+    //Navbar.jsx
+    <div className="profile">
+        <ArrowDropDown className="icon" />
+        <div className="options">
+            <span>Settings</span>
+            <span>Logout</span>
+        </div>
+    </div> /*
+b) Těmto položkám defaultně nastavíme že display:none a při :hover se zobrazí => POUŽIJEME PSEUDO PARENT SELECTOR = &:hover 
+    - Pomocí &:hover označujeme PARENT CLASS neboli rodičovskou třídu, což je v našem případě <div className="profile">. */
+    //navbar.scss
+    .profile{
+        .options{
+            display: none;
+        }
+        &:hover{
+            .options{
+                display: flex;
+            }
+        }
+    } /*
+"ZMĚNA BARVY PŘI SCROLLOVÁNÍ (top = transparent, scroll = black)"
+c) Budeme používat useState abychom určili zda jsme v pozici top / scroll.
+    - window.pageYOffset nám určuje zda jsme v pozici top (= 0), nebo právě scrollujeme po ose Y (> 0)
+    - window.onscroll() je window metoda která se zavolá PŘI KAŽDÉM SCROLLOVÁNÍ */ 
+    //Navbar.jsx
+    const [isScrolled, setIsScroled] = useState(false);
+    window.onscroll = () => {
+      setIsScroled(window.pageYOffset === 0 ? false : true);
+      return () => (window.onscroll = null);    //tímto zabráníme nekonečnému cyklení protože nasavím onscroll na null.
+    };
+    console.log(isScrolled); /* nejdříve bude false a při scrollování true
+d) Na základě state "isScrolled" z useState() se bude kondičně stylovat celý div. */
+    //Navbar.jsx
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}></div>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,15 @@
+import { Navbar } from "../components/navbar/Navbar";
 import "./home.scss";
-import { Abc } from "@mui/icons-material";
 
 export const Home = () => {
   return (
     <div className="home">
-      <Abc />
+      <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
     </div>
   );
 };
