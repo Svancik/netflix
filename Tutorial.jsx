cd8b8ad0665ff208c81777cceb0d43300f9bd33c
Svancik <thesvancik@gmail.com>
2022-06-21 21:43:07 +0200

    hover effect for items

--- Tutorial.jsx
+++ Tutorial.jsx
@@ -82,3 +82,41 @@ a) POSOUVÁNÍ UKÁZEK FILEM V NĚKOLIKA SEZNAMECH - použijeme k tomu "transfor
       }/*
       a2) pomocí isMoved ve state si nastavíme zda se zobrazí šipka - nechceme aby se zobrazovala šipka doleva pokud jsme na kraji
   
+------------------------------------------------------------------------------------------------------------------------------------------
+E1) REACT NETFLIX HOVER EFFECT
+
+a) Nastavíme si aby při hoveru se do state uložila informace že jsme najeli "HOVEROVALI" na daný film */
+// ListItem.jsx
+<div className="listItem" style={{ left: isHovered && index * 225 - 50 + index * 2.5 }} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)/>/*
+b) Pokud je splněna podmínka isHovered === true ve state, tak přehrajeme video a zobrazíme prvky (like, dislike, popis), jinak defaultně zobrazíme obrázek. */
+// ListItem.jsx
+<img
+src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+alt=""
+/>
+isHovered && (
+<>
+  <video src={trailer} autoPlay={true} loop />
+  <div className="itemInfo">
+    <div className="icons">
+      <PlayArrow />
+      <Add />
+      <ThumbUpAltOutlined />
+      <ThumbDownOutlined />
+    </div>
+    <div className="itemInfoTop">
+      <span>1 hour 14 mins</span>
+      <span className="limit">+ 16</span>
+      <span>1999</span>
+    </div>
+    <div className="desc">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut
+      soluta at repudiandae numquam perspiciatis. Inventore, molestias
+      mollitia.
+    </div>
+    <div className="genre">Action</div>
+  </div>
+</>
+)/*
+
+TIP: ALT + kursor = označení vícero řádků
